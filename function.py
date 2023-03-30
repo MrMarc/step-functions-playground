aa74177aad51120a597c7648d141c5d628ca4af2
Marc Boudreau <mboudreau@wisesystems.com>
2023-03-30 16:10:24 -0400

    VSCODE note

--- function.py
+++ function.py
@@ -1,12 +1,15 @@
 import json
 import datetime
 
+# THIS IS VSCODE Specific
 import ptvsd
 print('waiting for debugger... you should start it now')
 # Enable ptvsd on 0.0.0.0 address and on port 5890 that we'll connect later with our IDE
 ptvsd.enable_attach(address=('0.0.0.0', 5890), redirect_output=True)
 ptvsd.wait_for_attach()
 print('debugger attached')
+# END VSCODE SPECIFIC
+
 
 def lambda_handler(event, context):
     current_time = str(datetime.datetime.now())
